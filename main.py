a7ac7c7e87803e32f506e8901a887deb1e6d4b93
Christy Guan <cguan00@stuy.edu>
2020-02-29 21:04:01 -0500

    back to old testing

--- main.py
+++ main.py
@@ -4,16 +4,8 @@ from parser import *
 from matrix import *
 
 screen = new_screen()
-color = [ 200, 0, 200 ]
+color = [ 0, 255, 0 ]
 edges = []
 transform = new_matrix()
 
-parse_file( 'script0', edges, transform, screen, color )
-
-# make_translate(4, 5, 6)
-# make_scale(2,3,4)
-# make_rotZ(0)
-# make_rotY(180)
-# make_rotX(20)
-
-# print(edges)
+parse_file( 'script', edges, transform, screen, color )
